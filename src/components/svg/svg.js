8af82e5240b539fce8ffaf395fecd67684b1f822
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2026-03-17 13:36:34 +0100

    [Refactor] Adapt the props' names

--- src/components/svg/svg.js
+++ src/components/svg/svg.js
@@ -2,10 +2,10 @@
 // Started on July 2020
 // By Arnaud De Baerdemaeker
 
-const SVG = ({viewBox, svgClass, children}) => {
+const SVG = ({svgViewBox, svgClass, children}) => {
 	return (
 		<svg
-			viewBox={viewBox}
+			viewBox={svgViewBox}
 			xmlns={"http://www.w3.org/2000/svg"}
 			xmlnsXlink={"http://www.w3.org/1999/xlink"}
 			className={svgClass}
